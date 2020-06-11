8266147988cdc33a4b27bf799bd2142e073375ab
jinglun.li <jinglun.li@duodiankeji>
2020-06-11 18:15:28 +0800

    增加了min.js

--- dist/Duex.js
+++ dist/Duex.js
@@ -1,10 +1,311 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('core-js/modules/es6.promise'), require('core-js/modules/es6.object.to-string'), require('core-js/modules/es6.function.bind'), require('core-js/modules/es6.array.index-of'), require('core-js/modules/es6.array.for-each'), require('vue'), require('core-js/modules/web.dom.iterable'), require('core-js/modules/es6.array.iterator'), require('core-js/modules/es6.object.keys'), require('core-js/modules/es6.array.map'), require('core-js/modules/es6.array.is-array')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'core-js/modules/es6.promise', 'core-js/modules/es6.object.to-string', 'core-js/modules/es6.function.bind', 'core-js/modules/es6.array.index-of', 'core-js/modules/es6.array.for-each', 'vue', 'core-js/modules/web.dom.iterable', 'core-js/modules/es6.array.iterator', 'core-js/modules/es6.object.keys', 'core-js/modules/es6.array.map', 'core-js/modules/es6.array.is-array'], factory) :
-  (global = global || self, factory(global.Duex = {}, null, null, null, null, null, global.Vue));
-}(this, (function (exports, es6_promise, es6_object_toString, es6_function_bind, es6_array_indexOf, es6_array_forEach, Vue) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('vue')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'vue'], factory) :
+  (global = global || self, factory(global.Duex = {}, global.Vue));
+}(this, (function (exports, Vue) { 'use strict';
 
   Vue = Vue && Object.prototype.hasOwnProperty.call(Vue, 'default') ? Vue['default'] : Vue;
+
+  var LIBRARY = require('./_library');
+  var global = require('./_global');
+  var ctx = require('./_ctx');
+  var classof = require('./_classof');
+  var $export = require('./_export');
+  var isObject = require('./_is-object');
+  var aFunction = require('./_a-function');
+  var anInstance = require('./_an-instance');
+  var forOf = require('./_for-of');
+  var speciesConstructor = require('./_species-constructor');
+  var task = require('./_task').set;
+  var microtask = require('./_microtask')();
+  var newPromiseCapabilityModule = require('./_new-promise-capability');
+  var perform = require('./_perform');
+  var userAgent = require('./_user-agent');
+  var promiseResolve = require('./_promise-resolve');
+  var PROMISE = 'Promise';
+  var TypeError$1 = global.TypeError;
+  var process$1 = global.process;
+  var versions = process$1 && process$1.versions;
+  var v8 = versions && versions.v8 || '';
+  var $Promise = global[PROMISE];
+  var isNode = classof(process$1) == 'process';
+  var empty = function () { /* empty */ };
+  var Internal, newGenericPromiseCapability, OwnPromiseCapability, Wrapper;
+  var newPromiseCapability = newGenericPromiseCapability = newPromiseCapabilityModule.f;
+
+  var USE_NATIVE = !!function () {
+    try {
+      // correct subclassing with @@species support
+      var promise = $Promise.resolve(1);
+      var FakePromise = (promise.constructor = {})[require('./_wks')('species')] = function (exec) {
+        exec(empty, empty);
+      };
+      // unhandled rejections tracking support, NodeJS Promise without it fails @@species test
+      return (isNode || typeof PromiseRejectionEvent == 'function')
+        && promise.then(empty) instanceof FakePromise
+        // v8 6.6 (Node 10 and Chrome 66) have a bug with resolving custom thenables
+        // https://bugs.chromium.org/p/chromium/issues/detail?id=830565
+        // we can't detect it synchronously, so just check versions
+        && v8.indexOf('6.6') !== 0
+        && userAgent.indexOf('Chrome/66') === -1;
+    } catch (e) { /* empty */ }
+  }();
+
+  // helpers
+  var isThenable = function (it) {
+    var then;
+    return isObject(it) && typeof (then = it.then) == 'function' ? then : false;
+  };
+  var notify = function (promise, isReject) {
+    if (promise._n) return;
+    promise._n = true;
+    var chain = promise._c;
+    microtask(function () {
+      var value = promise._v;
+      var ok = promise._s == 1;
+      var i = 0;
+      var run = function (reaction) {
+        var handler = ok ? reaction.ok : reaction.fail;
+        var resolve = reaction.resolve;
+        var reject = reaction.reject;
+        var domain = reaction.domain;
+        var result, then, exited;
+        try {
+          if (handler) {
+            if (!ok) {
+              if (promise._h == 2) onHandleUnhandled(promise);
+              promise._h = 1;
+            }
+            if (handler === true) result = value;
+            else {
+              if (domain) domain.enter();
+              result = handler(value); // may throw
+              if (domain) {
+                domain.exit();
+                exited = true;
+              }
+            }
+            if (result === reaction.promise) {
+              reject(TypeError$1('Promise-chain cycle'));
+            } else if (then = isThenable(result)) {
+              then.call(result, resolve, reject);
+            } else resolve(result);
+          } else reject(value);
+        } catch (e) {
+          if (domain && !exited) domain.exit();
+          reject(e);
+        }
+      };
+      while (chain.length > i) run(chain[i++]); // variable length - can't use forEach
+      promise._c = [];
+      promise._n = false;
+      if (isReject && !promise._h) onUnhandled(promise);
+    });
+  };
+  var onUnhandled = function (promise) {
+    task.call(global, function () {
+      var value = promise._v;
+      var unhandled = isUnhandled(promise);
+      var result, handler, console;
+      if (unhandled) {
+        result = perform(function () {
+          if (isNode) {
+            process$1.emit('unhandledRejection', value, promise);
+          } else if (handler = global.onunhandledrejection) {
+            handler({ promise: promise, reason: value });
+          } else if ((console = global.console) && console.error) {
+            console.error('Unhandled promise rejection', value);
+          }
+        });
+        // Browsers should not trigger `rejectionHandled` event if it was handled here, NodeJS - should
+        promise._h = isNode || isUnhandled(promise) ? 2 : 1;
+      } promise._a = undefined;
+      if (unhandled && result.e) throw result.v;
+    });
+  };
+  var isUnhandled = function (promise) {
+    return promise._h !== 1 && (promise._a || promise._c).length === 0;
+  };
+  var onHandleUnhandled = function (promise) {
+    task.call(global, function () {
+      var handler;
+      if (isNode) {
+        process$1.emit('rejectionHandled', promise);
+      } else if (handler = global.onrejectionhandled) {
+        handler({ promise: promise, reason: promise._v });
+      }
+    });
+  };
+  var $reject = function (value) {
+    var promise = this;
+    if (promise._d) return;
+    promise._d = true;
+    promise = promise._w || promise; // unwrap
+    promise._v = value;
+    promise._s = 2;
+    if (!promise._a) promise._a = promise._c.slice();
+    notify(promise, true);
+  };
+  var $resolve = function (value) {
+    var promise = this;
+    var then;
+    if (promise._d) return;
+    promise._d = true;
+    promise = promise._w || promise; // unwrap
+    try {
+      if (promise === value) throw TypeError$1("Promise can't be resolved itself");
+      if (then = isThenable(value)) {
+        microtask(function () {
+          var wrapper = { _w: promise, _d: false }; // wrap
+          try {
+            then.call(value, ctx($resolve, wrapper, 1), ctx($reject, wrapper, 1));
+          } catch (e) {
+            $reject.call(wrapper, e);
+          }
+        });
+      } else {
+        promise._v = value;
+        promise._s = 1;
+        notify(promise, false);
+      }
+    } catch (e) {
+      $reject.call({ _w: promise, _d: false }, e); // wrap
+    }
+  };
+
+  // constructor polyfill
+  if (!USE_NATIVE) {
+    // 25.4.3.1 Promise(executor)
+    $Promise = function Promise(executor) {
+      anInstance(this, $Promise, PROMISE, '_h');
+      aFunction(executor);
+      Internal.call(this);
+      try {
+        executor(ctx($resolve, this, 1), ctx($reject, this, 1));
+      } catch (err) {
+        $reject.call(this, err);
+      }
+    };
+    // eslint-disable-next-line no-unused-vars
+    Internal = function Promise(executor) {
+      this._c = [];             // <- awaiting reactions
+      this._a = undefined;      // <- checked in isUnhandled reactions
+      this._s = 0;              // <- state
+      this._d = false;          // <- done
+      this._v = undefined;      // <- value
+      this._h = 0;              // <- rejection state, 0 - default, 1 - handled, 2 - unhandled
+      this._n = false;          // <- notify
+    };
+    Internal.prototype = require('./_redefine-all')($Promise.prototype, {
+      // 25.4.5.3 Promise.prototype.then(onFulfilled, onRejected)
+      then: function then(onFulfilled, onRejected) {
+        var reaction = newPromiseCapability(speciesConstructor(this, $Promise));
+        reaction.ok = typeof onFulfilled == 'function' ? onFulfilled : true;
+        reaction.fail = typeof onRejected == 'function' && onRejected;
+        reaction.domain = isNode ? process$1.domain : undefined;
+        this._c.push(reaction);
+        if (this._a) this._a.push(reaction);
+        if (this._s) notify(this, false);
+        return reaction.promise;
+      },
+      // 25.4.5.1 Promise.prototype.catch(onRejected)
+      'catch': function (onRejected) {
+        return this.then(undefined, onRejected);
+      }
+    });
+    OwnPromiseCapability = function () {
+      var promise = new Internal();
+      this.promise = promise;
+      this.resolve = ctx($resolve, promise, 1);
+      this.reject = ctx($reject, promise, 1);
+    };
+    newPromiseCapabilityModule.f = newPromiseCapability = function (C) {
+      return C === $Promise || C === Wrapper
+        ? new OwnPromiseCapability(C)
+        : newGenericPromiseCapability(C);
+    };
+  }
+
+  $export($export.G + $export.W + $export.F * !USE_NATIVE, { Promise: $Promise });
+  require('./_set-to-string-tag')($Promise, PROMISE);
+  require('./_set-species')(PROMISE);
+  Wrapper = require('./_core')[PROMISE];
+
+  // statics
+  $export($export.S + $export.F * !USE_NATIVE, PROMISE, {
+    // 25.4.4.5 Promise.reject(r)
+    reject: function reject(r) {
+      var capability = newPromiseCapability(this);
+      var $$reject = capability.reject;
+      $$reject(r);
+      return capability.promise;
+    }
+  });
+  $export($export.S + $export.F * (LIBRARY || !USE_NATIVE), PROMISE, {
+    // 25.4.4.6 Promise.resolve(x)
+    resolve: function resolve(x) {
+      return promiseResolve(LIBRARY && this === Wrapper ? $Promise : this, x);
+    }
+  });
+  $export($export.S + $export.F * !(USE_NATIVE && require('./_iter-detect')(function (iter) {
+    $Promise.all(iter)['catch'](empty);
+  })), PROMISE, {
+    // 25.4.4.1 Promise.all(iterable)
+    all: function all(iterable) {
+      var C = this;
+      var capability = newPromiseCapability(C);
+      var resolve = capability.resolve;
+      var reject = capability.reject;
+      var result = perform(function () {
+        var values = [];
+        var index = 0;
+        var remaining = 1;
+        forOf(iterable, false, function (promise) {
+          var $index = index++;
+          var alreadyCalled = false;
+          values.push(undefined);
+          remaining++;
+          C.resolve(promise).then(function (value) {
+            if (alreadyCalled) return;
+            alreadyCalled = true;
+            values[$index] = value;
+            --remaining || resolve(values);
+          }, reject);
+        });
+        --remaining || resolve(values);
+      });
+      if (result.e) reject(result.v);
+      return capability.promise;
+    },
+    // 25.4.4.4 Promise.race(iterable)
+    race: function race(iterable) {
+      var C = this;
+      var capability = newPromiseCapability(C);
+      var reject = capability.reject;
+      var result = perform(function () {
+        forOf(iterable, false, function (promise) {
+          C.resolve(promise).then(capability.resolve, reject);
+        });
+      });
+      if (result.e) reject(result.v);
+      return capability.promise;
+    }
+  });
+
+  // 19.1.3.6 Object.prototype.toString()
+  var classof$1 = require('./_classof');
+  var test = {};
+  test[require('./_wks')('toStringTag')] = 'z';
+  if (test + '' != '[object z]') {
+    require('./_redefine')(Object.prototype, 'toString', function toString() {
+      return '[object ' + classof$1(this) + ']';
+    }, true);
+  }
+
+  // 19.2.3.2 / 15.3.4.5 Function.prototype.bind(thisArg, args...)
+  var $export$1 = require('./_export');
+
+  $export$1($export$1.P, 'Function', { bind: require('./_bind') });
 
   function _classCallCheck(instance, Constructor) {
     if (!(instance instanceof Constructor)) {
@@ -101,6 +402,150 @@
       }
     };
   }
+
+  var $export$2 = require('./_export');
+  var $indexOf = require('./_array-includes')(false);
+  var $native = [].indexOf;
+  var NEGATIVE_ZERO = !!$native && 1 / [1].indexOf(1, -0) < 0;
+
+  $export$2($export$2.P + $export$2.F * (NEGATIVE_ZERO || !require('./_strict-method')($native)), 'Array', {
+    // 22.1.3.11 / 15.4.4.14 Array.prototype.indexOf(searchElement [, fromIndex])
+    indexOf: function indexOf(searchElement /* , fromIndex = 0 */) {
+      return NEGATIVE_ZERO
+        // convert -0 to +0
+        ? $native.apply(this, arguments) || 0
+        : $indexOf(this, searchElement, arguments[1]);
+    }
+  });
+
+  var $export$3 = require('./_export');
+  var $forEach = require('./_array-methods')(0);
+  var STRICT = require('./_strict-method')([].forEach, true);
+
+  $export$3($export$3.P + $export$3.F * !STRICT, 'Array', {
+    // 22.1.3.10 / 15.4.4.18 Array.prototype.forEach(callbackfn [, thisArg])
+    forEach: function forEach(callbackfn /* , thisArg */) {
+      return $forEach(this, callbackfn, arguments[1]);
+    }
+  });
+
+  var $iterators = require('./es6.array.iterator');
+  var getKeys = require('./_object-keys');
+  var redefine = require('./_redefine');
+  var global$1 = require('./_global');
+  var hide = require('./_hide');
+  var Iterators = require('./_iterators');
+  var wks = require('./_wks');
+  var ITERATOR = wks('iterator');
+  var TO_STRING_TAG = wks('toStringTag');
+  var ArrayValues = Iterators.Array;
+
+  var DOMIterables = {
+    CSSRuleList: true, // TODO: Not spec compliant, should be false.
+    CSSStyleDeclaration: false,
+    CSSValueList: false,
+    ClientRectList: false,
+    DOMRectList: false,
+    DOMStringList: false,
+    DOMTokenList: true,
+    DataTransferItemList: false,
+    FileList: false,
+    HTMLAllCollection: false,
+    HTMLCollection: false,
+    HTMLFormElement: false,
+    HTMLSelectElement: false,
+    MediaList: true, // TODO: Not spec compliant, should be false.
+    MimeTypeArray: false,
+    NamedNodeMap: false,
+    NodeList: true,
+    PaintRequestList: false,
+    Plugin: false,
+    PluginArray: false,
+    SVGLengthList: false,
+    SVGNumberList: false,
+    SVGPathSegList: false,
+    SVGPointList: false,
+    SVGStringList: false,
+    SVGTransformList: false,
+    SourceBufferList: false,
+    StyleSheetList: true, // TODO: Not spec compliant, should be false.
+    TextTrackCueList: false,
+    TextTrackList: false,
+    TouchList: false
+  };
+
+  for (var collections = getKeys(DOMIterables), i = 0; i < collections.length; i++) {
+    var NAME = collections[i];
+    var explicit = DOMIterables[NAME];
+    var Collection = global$1[NAME];
+    var proto = Collection && Collection.prototype;
+    var key;
+    if (proto) {
+      if (!proto[ITERATOR]) hide(proto, ITERATOR, ArrayValues);
+      if (!proto[TO_STRING_TAG]) hide(proto, TO_STRING_TAG, NAME);
+      Iterators[NAME] = ArrayValues;
+      if (explicit) for (key in $iterators) if (!proto[key]) redefine(proto, key, $iterators[key], true);
+    }
+  }
+
+  var addToUnscopables = require('./_add-to-unscopables');
+  var step = require('./_iter-step');
+  var Iterators$1 = require('./_iterators');
+  var toIObject = require('./_to-iobject');
+
+  // 22.1.3.4 Array.prototype.entries()
+  // 22.1.3.13 Array.prototype.keys()
+  // 22.1.3.29 Array.prototype.values()
+  // 22.1.3.30 Array.prototype[@@iterator]()
+  module.exports = require('./_iter-define')(Array, 'Array', function (iterated, kind) {
+    this._t = toIObject(iterated); // target
+    this._i = 0;                   // next index
+    this._k = kind;                // kind
+  // 22.1.5.2.1 %ArrayIteratorPrototype%.next()
+  }, function () {
+    var O = this._t;
+    var kind = this._k;
+    var index = this._i++;
+    if (!O || index >= O.length) {
+      this._t = undefined;
+      return step(1);
+    }
+    if (kind == 'keys') return step(0, index);
+    if (kind == 'values') return step(0, O[index]);
+    return step(0, [index, O[index]]);
+  }, 'values');
+
+  // argumentsList[@@iterator] is %ArrayProto_values% (9.4.4.6, 9.4.4.7)
+  Iterators$1.Arguments = Iterators$1.Array;
+
+  addToUnscopables('keys');
+  addToUnscopables('values');
+  addToUnscopables('entries');
+
+  // 19.1.2.14 Object.keys(O)
+  var toObject = require('./_to-object');
+  var $keys = require('./_object-keys');
+
+  require('./_object-sap')('keys', function () {
+    return function keys(it) {
+      return $keys(toObject(it));
+    };
+  });
+
+  var $export$4 = require('./_export');
+  var $map = require('./_array-methods')(1);
+
+  $export$4($export$4.P + $export$4.F * !require('./_strict-method')([].map, true), 'Array', {
+    // 22.1.3.15 / 15.4.4.19 Array.prototype.map(callbackfn [, thisArg])
+    map: function map(callbackfn /* , thisArg */) {
+      return $map(this, callbackfn, arguments[1]);
+    }
+  });
+
+  // 22.1.2.2 / 15.4.3.2 Array.isArray(arg)
+  var $export$5 = require('./_export');
+
+  $export$5($export$5.S, 'Array', { isArray: require('./_is-array') });
 
   function normalizeMap(map) {
     return Array.isArray(map) ? map.map(function (k) {
--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,8 @@
 import resolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import { uglify } from "rollup-plugin-uglify";
 
-export default {
+export default [{
     input: 'src/index.js',
     output: {
         file: 'dist/Duex.js',
@@ -15,4 +16,19 @@ export default {
             exclude: 'node_modules/**'
         })
     ]
-};
+}, {
+    input: 'src/index.js',
+    output: {
+        file: 'dist/Duex.min.js',
+        format: 'umd',
+        name: 'Duex',
+        sourcemap: true
+    },
+    plugins: [
+        resolve(),
+        babel({
+            exclude: 'node_modules/**'
+        }),
+        uglify()
+    ]
+}];
